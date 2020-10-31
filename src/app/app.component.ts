c7d274dca4b47cfed04cf2e346c2100d7be78214
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-31 21:22:30 +0300

    project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-routing';
+}
+
+/*
+A callback method that is invoked immediately 
+after the default change detector has checked the directive's data-bound properties for the first time, 
+and before any of the view or content children have been checked.
+It is invoked only once when the directive is instantiated.
+*/
