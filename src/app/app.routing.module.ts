f08827fc08fa30bc279693cb925398fbc812138b
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-11-04 22:18:43 +0300

    tracing router events

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -9,7 +9,7 @@ import { PageNotFoundComponent } from './page-not-found.component';
 			{ path: 'welcome', component: WelcomeComponent, data: [{ pageTitle: "Welcome to APM" }] },
 			{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
 			{ path: '**', component: PageNotFoundComponent },
-		])
+		], { enableTracing: true })
 	],
 	exports: [
 		RouterModule // app.module components / directives should access RouterModule
