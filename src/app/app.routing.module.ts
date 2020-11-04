8a42ff5d61dd91eb097b685db77bfd1ddb422db2
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-11-05 01:24:34 +0300

    PreloadAllModules

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
@@ -11,7 +11,7 @@ import { AuthGuard } from './user/auth.guard';
 			{ path: 'products', canLoad: [AuthGuard], loadChildren: () => import('./products/product.module').then(m => m.ProductModule) },
 			{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
 			{ path: '**', component: PageNotFoundComponent },
-		])
+		], { preloadingStrategy: PreloadAllModules })
 	],
 	exports: [
 		RouterModule // app.module components / directives should access RouterModule
