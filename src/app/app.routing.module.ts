befbdb41412043b1256b69ccb39698522836b8af
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-11-05 01:33:04 +0300

    SelectiveStrategy implements PreloadingStrategy

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { SelectiveStrategy } from './selective-strategy.service';
 import { AuthGuard } from './user/auth.guard';
 
 @NgModule({
 	imports: [
 		RouterModule.forRoot([
 			{ path: 'welcome', component: WelcomeComponent, data: [{ pageTitle: "Welcome to APM" }] },
-			{ path: 'products', canActivate: [AuthGuard], loadChildren: () => import('./products/product.module').then(m => m.ProductModule) },
+			{ 
+				path: 'products', 
+				canActivate: [AuthGuard], 
+				loadChildren: () => import('./products/product.module')
+										.then(m => m.ProductModule) ,
+				data: { preload: false }
+			},
 			{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
 			{ path: '**', component: PageNotFoundComponent },
-		], { preloadingStrategy: PreloadAllModules })
+		], { preloadingStrategy: SelectiveStrategy })
 	],
 	exports: [
 		RouterModule // app.module components / directives should access RouterModule
