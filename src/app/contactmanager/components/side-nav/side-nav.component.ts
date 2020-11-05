b7992750e43b0ed9d2e012aeda87db4be53ab92b
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-11-06 00:55:39 +0300

    BreakpointObserver

--- src/app/contactmanager/components/side-nav/side-nav.component.ts
+++ src/app/contactmanager/components/side-nav/side-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-side-nav',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-	showFiller: true;
+	showFiller: boolean;
+	screenIsSmall: boolean;
 	
-	constructor() { }
+	constructor(private breakpointObserver: BreakpointObserver) { }
 	
-	ngOnInit(): void { }
+	ngOnInit(): void {
+		this.breakpointObserver
+			//.observe([Breakpoints.XSmall])
+			.observe([` (max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+			.subscribe((state: BreakpointState) => {
+				console.log('state :>> ', state);
+				this.screenIsSmall = state.matches;
+			})
+	}
 
 }
