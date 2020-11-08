ba712ce63ce25e17616087f230fb61db6e20b12d
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-11-08 15:16:12 +0300

    StoreModule configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/page-not-found/shared.module';
 import { HomeComponent } from './home/home.component';
 
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { productReducer } from './products/state/product.reducer';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-	  BrowserModule,
-	  AppRoutingModule,
-	  SharedModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		SharedModule,
+		StoreModule.forRoot({}, {}),
+		StoreModule.forFeature('products', productReducer),
+		StoreDevtoolsModule.instrument({
+			name: 'APM Demo App Dev Tools',
+			maxAge: 25,
+			logOnly: environment.production
+		})
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
