b15c52c96128705947edb8100c0ac259ebc0ac8e
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-11-08 15:16:58 +0300

    reducer in the component

--- src/app/products/models/product.ts
+++ src/app/products/models/product.ts
@@ -1,5 +1,7 @@
 export interface IProduct {
 	id: string;
 	name: string;
+	desc: string;
 	price: string;
+	thumb: string;
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { IProduct } from './models/product';
 import { ProductService } from './product.service';
+import { State } from './state/product.reducer';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+	selector: 'app-products',
+	templateUrl: './products.component.html',
+	styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+	showThumbs: boolean;
 
-  constructor(private service: ProductService) { }
+	constructor(
+		private service: ProductService,
+		private store: Store<State>
+	) { }
 
-  product$: Observable<IProduct[]>;
+	product$: Observable<IProduct[]>;
 
-  ngOnInit(): void {
-	  this.product$ = this.service.listAllProducts();
-   }
+	ngOnInit(): void {
+		this.product$ = this.service.listAllProducts();
+		this.store.select('products').subscribe(products => {
+			console.log('products :>> ', products);
+			if (products) {
+				this.showThumbs = products.showThumbnail;
+			}
+		});
+	}
+
+	toggleThumb() {
+		this.showThumbs = !this.showThumbs;
+	}
 
 }
