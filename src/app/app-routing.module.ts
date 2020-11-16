472526a0adf0ef78c788e00f009db1fdae0a4e94
ozgursarikamis <ozgursarikamis@gmail.com>
2020-11-17 00:19:39 +0300

    routing module configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesDetailComponent } from './movies-detail/movies-detail.component';
+import { MoviesComponent } from './movies/movies.component';
+import { SeriesDetailComponent } from './series-detail/series-detail.component';
+import { SeriesComponent } from './series/series.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'movies',
+    component: MoviesComponent,
+  },
+  {
+    path: 'movies/:movieId',
+    component: MoviesDetailComponent,
+  },
+  {
+    path: 'series',
+    component: SeriesComponent,
+  },
+  {
+    path: 'series/:seriesId',
+    component: SeriesDetailComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
