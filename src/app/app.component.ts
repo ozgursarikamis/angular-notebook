7ff4299119342257ce47f394f5a4cbde5c570c33
ozgursarikamis <ozgursarikamis@gmail.com>
2020-11-16 14:13:15 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,3 @@ import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/c
 export class AppComponent {
   title = 'angular';
 }
-
-/*
-A callback method that is invoked immediately 
-after the default change detector has checked the directive's data-bound properties for the first time, 
-and before any of the view or content children have been checked.
-It is invoked only once when the directive is instantiated.
-*/
