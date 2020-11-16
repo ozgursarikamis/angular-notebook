ba0e53f2253ac41eb5d6f7c1c1588d5b3cc97143
ozgursarikamis <ozgursarikamis@gmail.com>
2020-11-17 00:39:06 +0300

    read router store from a component

--- src/app/movies-detail/movies-detail.component.ts
+++ src/app/movies-detail/movies-detail.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { movies } from './../app.constants';
+import { StoreRootState } from '../store';
+import { select, Store } from '@ngrx/store';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { getRouterState } from 'src/app/store/selectors';
 
 @Component({
   selector: 'app-movies-detail',
   templateUrl: './movies-detail.component.html',
   styleUrls: ['./movies-detail.component.scss']
 })
-export class MoviesDetailComponent implements OnInit {
+export class MoviesDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  movieId: string;
+  movie: any;
+
+  private subscriptions: { [key: string]: any } = {};
+
+  constructor(private store: Store<StoreRootState>) { }
 
   ngOnInit(): void {
+    this.subscriptions.routerSelector = this.store
+      .pipe(select(getRouterState))
+      .subscribe((route) => {
+        const movieId = route.state.params.movieId;
+        this.movie = movies.find(x => x.id === movieId);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.routerSelector.unsubscribe();
   }
 
 }
--- src/app/store/selectors.ts
+++ src/app/store/selectors.ts
@@ -1,11 +1,10 @@
-import { routerReducer } from '@ngrx/router-store';
 import * as fromRouter from '@ngrx/router-store';
-import { ActionReducerMap } from '@ngrx/store';
+import { createSelector } from '@ngrx/store';
+import { StoreRootState } from '.';
 
-export interface StoreRootState {
-  router: fromRouter.RouterReducerState<any>;
-}
+export const getRouterState = (state: StoreRootState) => state.router;
 
-export const reducers: ActionReducerMap<StoreRootState> = {
-  router: routerReducer
-}
+export const getCurrentRouteState = createSelector(
+  getRouterState,
+  (state: fromRouter.RouterReducerState) => state.state
+);
