efe34c6c0d247c519a18e0a15bfeea7aaa19aa52
OZGUR SARIKAMIS <ozgursarikamis@gmail.com>
2020-11-27 00:55:53 +0300

    imorting whole json file from assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as test from "../assets/data/test.json";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  constructor() {
+	  console.log('test :>> ', (test as any).default);
+  }
 }
