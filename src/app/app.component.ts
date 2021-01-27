348c2cb07a2392c33e5a105c2fa70fafda6dfe6d
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2021-01-28 01:05:20 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,47 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { SignalRService } from './services/signalr-service.service';
+import { SignalRService } from './services/signal-r.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-	title = 'angular';
+  public chartOptions: any = {
+    scaleShowVerticalLines: true,
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+  public chartLabels: string[] = ['Real time data for the chart'];
+  public chartType: string = 'bar';
+  public chartLegend: boolean = true;
+  public colors: any[] = [{ backgroundColor: '#5491DA' }, { backgroundColor: '#E74C3C' }, { backgroundColor: '#82E0AA' }, { backgroundColor: '#E5E7E9' }]
 
-	public chartOptions: any = {
-		scaleShowVerticalLines: true,
-		responsive: true,
-		scales: {
-		  yAxes: [{
-			ticks: {
-			  beginAtZero: true
-			}
-		  }]
-		}
-	  };
+  constructor(public signalRService: SignalRService, private http: HttpClient) { }
 
-	public chartLabels: string[] = ['Real time data for the chart'];
-	public chartType: string = 'bar';
-	public chartLegend: boolean = true;
-	public colors: any[] = [
-		{ backgroundColor: '#5491DA' }, 
-		{ backgroundColor: '#E74C3C' }, 
-		{ backgroundColor: '#82E0AA' }, 
-		{ backgroundColor: '#E5E7E9' }
-	];
-	
-	constructor(public signalRService: SignalRService, private http: HttpClient) { }
+  ngOnInit() {
+    this.signalRService.startConnection();
+    this.signalRService.addTransferChartDataListener();
+    this.signalRService.addBroadcastChartDataListener();
+    this.startHttpRequest();
+  }
 
-	ngOnInit() {
-	  this.signalRService.startConnection();
-	  this.signalRService.addTransferChartDataListener();
-	  this.signalRService.addBroadcastChartDataListener();
-	  this.startHttpRequest();
-	}
-  
-	private startHttpRequest = () => {
-	  this.http.get('http://localhost:5001/api/chart')
-		.subscribe(res => {
-		  console.log(res);
-		})
-	}
-  
-	public chartClicked = (event) => {
-	  console.log(event);
-	  this.signalRService.broadcastChartData();
-	}
+  private startHttpRequest = () => {
+    this.http.get('https://localhost:5001/api/chart')
+      .subscribe(res => {
+        console.log(res);
+      })
+  }
+
+  public chartClicked = (event) => {
+    console.log(event);
+    this.signalRService.broadcastChartData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +11,8 @@ import { ChartsModule } from 'ng2-charts';
   ],
   imports: [
     BrowserModule,
-	AppRoutingModule,
-	ChartsModule,
-	HttpClientModule
+    ChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
